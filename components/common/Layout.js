0dbc63235d2d06540fcf4accb3d46b5712a99b24
Lakshya Kumar <lakshyak983@gmail.com>
2021-08-06 01:56:35 +0530

    another fix

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -1,12 +1,7 @@
-// import Navbar from "./navbar";
-// import Footer from "./footer";
-
 export default function Layout({ children }) {
     return (
         <>
-            {/* <Navbar /> */}
             <main>{children}</main>
-            {/* <Footer /> */}
         </>
     );
 }
